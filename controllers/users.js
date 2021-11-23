78c78fff70b325fc7ea3d0c0aa7efb0b01b31c92
shiva0501 <shiva46574@gmail.com>
2021-11-23 19:55:56 +0530

    ignore nodemodules

---
+++ controllers/users.js
@@ -0,0 +1,45 @@
+import { v4 as uuidv4 } from 'uuid';
+
+let users = [];
+
+export const getUsers = (req, res) => {
+    res.send(users);
+}
+
+export const createUser = (req, res) => {
+    const user = req.body;
+
+    users.push({ ...user, id: uuidv4() });
+
+    res.send(`User with the name ${user.firstName} is added to database`);
+}
+
+export const getUser = (req, res) => {
+    const userId = req.params.id;
+
+    const foundUser = users.find((user) => user.id === userId)
+    
+    res.send(foundUser);
+}
+
+export const deleteUser = (req, res) => {
+    const { id } = req.params;
+
+    users = users.filter((user) => user.id !== id);
+
+    res.send(`user with id ${id} is deleted from database`);
+}
+
+export const updateUser = (req, res) => {
+    const { id } = req.params;
+    const { firstName, lastName, age } = req.body;
+
+    const user = users.find((user) => user.id === id);
+
+    if(firstName) user.firstName = firstName;
+    if(lastName) user.lastName = lastName;
+    if(age) user.age = age;
+
+    res.send(`User with id ${id} has been updated`);
+
+}
---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import userRoutes from './routes/user.js';
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+app.use('/users', userRoutes);
+
+app.get('/', (req,res) => {
+    res.send('Hello from home page');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: http://localhost:${PORT}`);
+})
